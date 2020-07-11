77038e2f71431c41efe05924b51e0f1b1d0f3b2a
Celso Crivelaro <celsocrivelaro@gmail.com>
2020-07-11 10:12:51 -0300

    Limites de representação

---
+++ limites.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+// https://www.tutorialspoint.com/c_standard_library/limits_h.htm
+#include <limits.h> // contantes com os valores máximos
+
+int main() {
+   int overflow = 0; 
+   
+   // INT_MAX = +2147483647 ou 2^32/2 signed_int
+   overflow =  INT_MAX + 1; 
+   
+   return 0;
+}
